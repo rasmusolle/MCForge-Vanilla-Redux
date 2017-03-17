e82f010fe81cdc7c39e516ca9fdf382624eb65e7
Rasmus <rasmusbamse@hotmail.com>
2017-03-17 18:20:04 +0100

    Cleanup MonoTorrent

--- queue/MainLoop.cs
+++ queue/MainLoop.cs
@@ -28,125 +28,42 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using MonoTorrent.Common;
 
 namespace MonoTorrent.Client
 {
     public delegate void MainLoopResult(object result);
     public delegate object MainLoopJob();
     public delegate void MainLoopTask();
-    public delegate bool TimeoutTask();
 
     public class MainLoop
     {
-        private class DelegateTask : ICacheable
+        private class SchedulerTask
         {
-            private ManualResetEvent handle;
-            private bool isBlocking;
-            private MainLoopJob job;
-            private object jobResult;
-            private Exception storedException;
-            private MainLoopTask task;
-            private TimeoutTask timeout;
-            private bool timeoutResult;
-
-            public bool IsBlocking
-            {
-                get { return isBlocking; }
-                set { isBlocking = value; }
-            }
-
-            public MainLoopJob Job
-            {
-                get { return job; }
-                set { job = value; }
-            }
-
-            public Exception StoredException
-            {
-                get { return storedException; }
-                set { storedException = value; }
-            }
-
-            public MainLoopTask Task
-            {
-                get { return task; }
-                set { task = value; }
-            }
-
-            public TimeoutTask Timeout
-            {
-                get { return timeout; }
-                set { timeout = value; }
-            }
-
-            public object JobResult
-            {
-                get { return jobResult; }
-            }
-
-            public bool TimeoutResult
-            {
-                get { return timeoutResult; }
-            }
-
-            public ManualResetEvent WaitHandle
-            {
-                get { return handle; }
-            }
-
-            public DelegateTask()
-            {
-                handle = new ManualResetEvent(false);
-            }
+            public Exception StoredException;
+            public MainLoopTask Task;
 
             public void Execute()
             {
                 try
                 {
-                    if (job != null)
-                        jobResult = job();
-                    else if (task != null)
-                        task();
-                    else if (timeout != null)
-                        timeoutResult = timeout();
+                    Task();
                 }
                 catch (Exception ex)
                 {
-                    storedException = ex;
-
-                    // FIXME: I assume this case can't happen. The only user interaction
-                    // with the mainloop is with blocking tasks. Internally it's a big bug
-                    // if i allow an exception to propagate to the mainloop.
-                    if (!IsBlocking)
-                        throw;
+                    StoredException = ex;
+                    throw;
                 }
-                finally
-                {
-                    handle.Set();
-                }
-            }
-
-            public void Initialise()
-            {
-                isBlocking = false;
-                job = null;
-                jobResult = null;
-                storedException = null;
-                task = null;
-                timeout = null;
-                timeoutResult = false;
             }
         }
 
         AutoResetEvent handle = new AutoResetEvent(false);
-        ICache<DelegateTask> cache = new Cache<DelegateTask>(true).Synchronize();
-        Queue<DelegateTask> tasks = new Queue<DelegateTask>();
+        Queue<SchedulerTask> tasks = new Queue<SchedulerTask>();
         internal Thread thread;
 
         public MainLoop(string name)
         {
             thread = new Thread(Loop);
+            thread.Name = name;
             thread.IsBackground = true;
             thread.Start();
         }
@@ -155,8 +72,7 @@ namespace MonoTorrent.Client
         {
             while (true)
             {
-                DelegateTask task = null;
-
+                SchedulerTask task = null;
                 lock (tasks)
                 {
                     if (tasks.Count > 0)
@@ -169,79 +85,23 @@ namespace MonoTorrent.Client
                 }
                 else
                 {
-                    bool reuse = !task.IsBlocking;
                     task.Execute();
-                    if (reuse)
-                        cache.Enqueue(task);
                 }
                 Thread.Sleep(10);
             }
         }
 
-        private void Queue(DelegateTask task)
+        /// <summary> Queues an action that is asynchronously executed. </summary>
+        public void Queue(MainLoopTask action)
         {
-            Queue(task, Priority.Normal);
-        }
+            SchedulerTask task = new SchedulerTask();
+            task.Task = action;
 
-        private void Queue(DelegateTask task, Priority priority)
-        {
             lock (tasks)
             {
                 tasks.Enqueue(task);
                 handle.Set();
             }
-        }
-
-        public void Queue(MainLoopTask task)
-        {
-            DelegateTask dTask = cache.Dequeue();
-            dTask.Task = task;
-            Queue(dTask);
-        }
-
-        public void QueueWait(MainLoopTask task)
-        {
-            DelegateTask dTask = cache.Dequeue();
-            dTask.Task = task;
-            try
-            {
-                QueueWait(dTask);
-            }
-            finally
-            {
-                cache.Enqueue(dTask);
-            }
-        }
-
-        public object QueueWait(MainLoopJob task)
-        {
-            DelegateTask dTask = cache.Dequeue();
-            dTask.Job = task;
-
-            try
-            {
-                QueueWait(dTask);
-                return dTask.JobResult;
-            }
-            finally
-            {
-                cache.Enqueue(dTask);
-            }
-        }
-
-        private void QueueWait(DelegateTask t)
-        {
-            t.WaitHandle.Reset();
-            t.IsBlocking = true;
-            if (Thread.CurrentThread == thread)
-                t.Execute();
-            else
-                Queue(t, Priority.Highest);
-
-            t.WaitHandle.WaitOne();
-
-            if (t.StoredException != null)
-                throw new TorrentException("Exception in mainloop", t.StoredException);
         }
     }
 }
