16da2f14fda9155eeb1c231981e68f93fe43f3a1
Rasmus <rasmusbamse@hotmail.com>
2017-04-01 14:55:07 +0200

    cleanup PlayerCollection.cs

--- Player/PlayerCollection.cs
+++ Player/PlayerCollection.cs
@@ -24,10 +24,7 @@ namespace MCForge
     {
         protected IPlayerViewBuilder _viewBuilder;
 
-        public PlayerCollection(IPlayerViewBuilder viewBuilder)
-        {
-            _viewBuilder = viewBuilder;
-        }
+        public PlayerCollection(IPlayerViewBuilder viewBuilder) { _viewBuilder = viewBuilder; }
 
         #region ITypedList Members
 
@@ -35,49 +32,26 @@ namespace MCForge
 
         public PropertyDescriptorCollection GetItemProperties(PropertyDescriptor[] listAccessors)
         {
-            if (_props == null)
-            {
-                _props = _viewBuilder.GetView();
-            }
+            if (_props == null) { _props = _viewBuilder.GetView(); }
             return _props;
         }
 
-        public string GetListName(PropertyDescriptor[] listAccessors)
-        {
-            return ""; // was used by 1.1 datagrid
-        }
+        public string GetListName(PropertyDescriptor[] listAccessors) { return ""; }
 
         #endregion
     }
 
-    public interface IPlayerViewBuilder
-    {
-        PropertyDescriptorCollection GetView();
-    }
+    public interface IPlayerViewBuilder { PropertyDescriptorCollection GetView(); }
 
     public class PlayerListView : IPlayerViewBuilder
     {
         public PropertyDescriptorCollection GetView()
         {
             List<PropertyDescriptor> props = new List<PropertyDescriptor>();
-            /*PlayerMethodDelegate del = delegate(Player p)
-            {
-                return p.name;
-            };*/
+
             props.Add(new PlayerMethodDescriptor("Name", p => p.name, typeof(string)));
-
             props.Add(new PlayerMethodDescriptor("Map", p => p.level.name, typeof(string)));
-
             props.Add(new PlayerMethodDescriptor("Rank", p => p.group.name, typeof(string)));
-
-            props.Add(new PlayerMethodDescriptor("Status", p =>
-                      {
-                          if (p.hidden)
-                              return "hidden";
-                          if (Server.afkset.Contains(p.name))
-                              return "afk";
-                          return "active";
-                      }, typeof(string)));
 
             PropertyDescriptor[] propArray = new PropertyDescriptor[props.Count];
             props.CopyTo(propArray);
@@ -93,46 +67,19 @@ namespace MCForge
         protected PlayerMethodDelegate _method;
         protected Type _methodReturnType;
 
-        public PlayerMethodDescriptor(string name, PlayerMethodDelegate method,
-         Type methodReturnType)
-            : base(name, null)
+        public PlayerMethodDescriptor(string name, PlayerMethodDelegate method, Type methodReturnType) : base(name, null)
         {
             _method = method;
             _methodReturnType = methodReturnType;
         }
 
-        public override object GetValue(object component)
-        {
-            return _method((Player)component);
-        }
-
-        public override Type ComponentType
-        {
-            get { return typeof(Player); }
-        }
-
-        public override Type PropertyType
-        {
-            get { return _methodReturnType; }
-        }
-
-        public override bool CanResetValue(object component)
-        {
-            return false;
-        }
-
+        public override object GetValue(object component) { return _method((Player)component); }
+        public override Type ComponentType { get { return typeof(Player); } }
+        public override Type PropertyType { get { return _methodReturnType; } }
+        public override bool CanResetValue(object component) { return false; }
         public override void ResetValue(object component) { }
-
-        public override bool IsReadOnly
-        {
-            get { return true; }
-        }
-
+        public override bool IsReadOnly { get { return true; } }
         public override void SetValue(object component, object value) { }
-
-        public override bool ShouldSerializeValue(object component)
-        {
-            return false;
-        }
+        public override bool ShouldSerializeValue(object component) { return false; }
     }
 }
