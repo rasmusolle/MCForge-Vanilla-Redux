ce0b5fda824235e721d79a8e3760d60fd25e4463
Rasmus <rasmusbamse@hotmail.com>
2017-04-01 14:56:47 +0200

    cleanup PlayerList.cs

--- Player/PlayerList.cs
+++ Player/PlayerList.cs
@@ -21,21 +21,15 @@ namespace MCForge
 {
     public class PlayerList
     {
-        //public string name;
         public Group group;
         List<string> players = new List<string>();
         public PlayerList() { }
         public void Add(string p) { players.Add(p.ToLower()); }
-        public bool Remove(string p)
-        {
-            return players.Remove(p.ToLower());
-        }
+        public bool Remove(string p) { return players.Remove(p.ToLower()); }
         public bool Contains(string p) { return players.Contains(p.ToLower()); }
         public List<string> All() { return new List<string>(players); }
         public void Save(string path) { Save(path, true); }
-        public void Save() {
-            Save(group.fileName); 
-        }
+        public void Save() { Save(group.fileName); }
         public void Save(string path, bool console)
         {
             StreamWriter file = File.CreateText("ranks/" + path);
@@ -49,14 +43,13 @@ namespace MCForge
             PlayerList list = new PlayerList();
             list.group = groupName;
             if (File.Exists(path))
-            {
                 foreach (string line in File.ReadAllLines(path)) { list.Add(line); }
-            }
             else
             {
                 File.Create(path).Close();
                 Server.s.Log("CREATED NEW: " + path);
-            } return list;
+            }
+            return list;
         }
     }
 }
