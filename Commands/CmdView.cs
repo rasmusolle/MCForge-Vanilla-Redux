1f28e4c371012accb8ea58d4563e488699142a7a
Rasmus <rasmusbamse@hotmail.com>
2017-04-03 19:40:35 +0200

    rewrite /view

--- Commands/CmdView.cs
+++ Commands/CmdView.cs
@@ -1,4 +1,20 @@
-﻿//TODO: Cleanup and put license
+﻿/*
+	Copyright 2017 MCSpleef
+	
+	Dual-licensed under the	Educational Community License, Version 2.0 and
+	the GNU General Public License, Version 3 (the "Licenses"); you may
+	not use this file except in compliance with the Licenses. You may
+	obtain a copy of the Licenses at
+	
+	http://www.opensource.org/licenses/ecl2.php
+	http://www.gnu.org/licenses/gpl-3.0.html
+	
+	Unless required by applicable law or agreed to in writing,
+	software distributed under the Licenses are distributed on an "AS IS"
+	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+	or implied. See the Licenses for the specific language governing
+	permissions and limitations under the Licenses.
+*/
 using System;
 using System.IO;
 namespace MCForge.Commands
@@ -9,36 +25,23 @@ namespace MCForge.Commands
 		public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
 		public override void Use(Player p, string message)
 		{
-			if (!Directory.Exists("extra/text/")) Directory.CreateDirectory("extra/text");
+			if (!Directory.Exists("text/view")) Directory.CreateDirectory("text/view");
 			if (message == "")
 			{
-				DirectoryInfo di = new DirectoryInfo("extra/text/");
+				DirectoryInfo di = new DirectoryInfo("text/view");
 				string allFiles = "";
 				foreach (FileInfo fi in di.GetFiles("*.txt"))
 				{
 					try
 					{
-						string firstLine = File.ReadAllLines("extra/text/" + fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length) + ".txt")[0];
-						if (firstLine[0] == '#')
-						{
-							if (Group.Find(firstLine.Substring(1)).Permission <= p.group.Permission)
-							{
-								allFiles += ", " + fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
-							}
-						}
-						else
-						{
-							allFiles += ", " + fi.Name;
-						}
+						string firstLine = File.ReadAllLines("text/view" + fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length) + ".txt")[0];
+						allFiles += ", " + fi.Name;
 					} catch (Exception e) { Server.ErrorLog(e); Player.SendMessage(p, "Error"); }
 				}
 
-				if (allFiles == "")
-				{
+				if (allFiles == "") {
 					Player.SendMessage(p, "No files are viewable by you");
-				}
-				else
-				{
+				} else {
 					Player.SendMessage(p, "Available files:");
 					Player.SendMessage(p, allFiles.Remove(0, 2));
 				}
@@ -54,32 +57,12 @@ namespace MCForge.Commands
 				}
 				if (who == null) who = p;
 
-				if (File.Exists("extra/text/" + message + ".txt"))
+				if (File.Exists("text/view" + message + ".txt"))
 				{
 					try
 					{
-						string[] allLines = File.ReadAllLines("extra/text/" + message + ".txt");
-						if (allLines[0][0] == '#')
-						{
-							if (Group.Find(allLines[0].Substring(1)).Permission <= p.group.Permission)
-							{
-								for (int i = 1; i < allLines.Length; i++)
-								{
-									Player.SendMessage(who, allLines[i]);
-								}
-							}
-							else
-							{
-								Player.SendMessage(p, "You cannot view this file");
-							}
-						}
-						else
-						{
-							for (int i = 1; i < allLines.Length; i++)
-							{
-								Player.SendMessage(who, allLines[i]);
-							}
-						}
+						string[] allLines = File.ReadAllLines("text/view" + message + ".txt");
+						for (int i = 1; i < allLines.Length; i++) Player.SendMessage(who, allLines[i]);
 					} catch { Player.SendMessage(p, "An error occurred when retrieving the file"); }
 				}
 				else
