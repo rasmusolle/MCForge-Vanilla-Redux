c44151cdf9f34a8fbea75db6c2fe573d6c49ca24
Rasmus <rasmusbamse@hotmail.com>
2017-04-06 21:18:49 +0200

    cleanup /rank

--- Commands/CmdRank.cs
+++ Commands/CmdRank.cs
@@ -27,9 +27,7 @@ namespace MCForge.Commands
 			if (message.Split(' ').Length < 2) { Help(p); return; }
 			Player who = Player.Find(message.Split(' ')[0]);
 			Group newRank = Group.Find(message.Split(' ')[1]);
-			string msgGave;
 
-			if (message.Split(' ').Length > 2) msgGave = message.Substring(message.IndexOf(' ', message.IndexOf(' ') + 1)); else msgGave = "Congratulations!";
 			if (newRank == null) { Player.SendMessage(p, "Could not find specified rank."); return; }
 
 			Group bannedGroup = Group.findPerm(LevelPermission.Banned);
@@ -86,20 +84,16 @@ namespace MCForge.Commands
 				newRank.playerList.Save();
 
 				Player.GlobalChat(who, who.color + who.name + Server.DefaultColor + "'s rank was set to " + newRank.color + newRank.name, false);
-				Player.GlobalChat(null, "&6" + msgGave, false);
 				who.group = newRank;
 				who.color = who.group.color;
 				Player.GlobalDie(who, false);
-				who.SendMessage("You are now ranked " + newRank.color + newRank.name + Server.DefaultColor + ", type /help for your new set of commands.");
+				who.SendMessage("You are now ranked " + newRank.color + newRank.name + Server.DefaultColor + ".");
 				Player.GlobalSpawn(who, who.pos[0], who.pos[1], who.pos[2], who.rot[0], who.rot[1], false);
 			}
 		}
 		public override void Help(Player p)
 		{
-			Player.SendMessage(p, "/rank <player> <rank> <yay> - Sets or returns a players rank.");
-			Player.SendMessage(p, "You may use /rank as a shortcut");
-			Player.SendMessage(p, "Valid Ranks are: " + Group.concatList(true, true));
-			Player.SendMessage(p, "<yay> is a celebratory message");
+			Player.SendMessage(p, "/rank <player> <rank> <yay> - Sets a players rank.");
 		}
 	}
 }
